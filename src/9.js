8584c39498877090487db41304ceb5bb3c255192
yvettlokos229 <yvettlokos229@gmail.com>
2025-03-14 22:21:15 +0000

    Add 9.js

---
+++ src/9.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const port = 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port);
