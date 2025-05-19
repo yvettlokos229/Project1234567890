98aca977498e015f2f3d86723ee6e241398c248f
yvettlokos229 <yvettlokos229@gmail.com>
2025-05-19 12:19:26 +0000

    Add 36.js

---
+++ src/36.js
@@ -0,0 +1,19 @@
+// 项目1234567890（A综合学校项目）的代码实现
+
+// 假设这是一个函数，用于计算两个数字的和
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// 假设这是另一个函数，用于比较两个数字并返回较大的一个
+function compareNumbers(num1, num2) {
+    if (num1 > num2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 调用上述函数以计算和与较大的数的比值
+const result = addNumbers(5, 3);
+console.log(`两个数字的和是: ${result}`);
